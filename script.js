9ed7ec7731fb81cc5685283a593a7d27dcfb1117
peter khella <peterkhella@peters-MacBook-Pro.local>
2022-02-06 12:03:43 -0800

    cloned repos and pushed email box

--- script.js
+++ script.js
@@ -126,3 +126,15 @@ function factSwither(event) {
 }
 
 factSwither();
+
+
+document.getElementById("email").addEventListener("click", function () {
+    document.querySelector(".email-form").classList.add("open");
+    // document.getElementById("hamburger").classList.add("off");
+    // document.getElementById("hamburger-close").classList.remove("off");
+});
+document.getElementById("email-close").addEventListener("click", function () {
+    document.querySelector(".email-form").classList.remove("open");
+    // document.getElementById("hamburger").classList.remove("off");
+    // document.getElementById("hamburger-close").classList.add("off");
+});
